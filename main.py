8baafc5e6cb2cce80bccabcd979f8703318f2847
korqemer <nmiras99@gmail.com>
2023-05-08 23:39:38 +0600

    background added

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ from sys import exit
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 400))
+# display surface -> x, y
+screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Fireboy and Watergirl")
+clock = pygame.time.Clock()
+
+# added surface
+backgorund_surface = pygame.image.load("graphics/background.png")
 
 
 running = True
-
-
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
+    screen.blit(backgorund_surface, (0, 0))
+
 
     pygame.display.update()
+    clock.tick(60)
